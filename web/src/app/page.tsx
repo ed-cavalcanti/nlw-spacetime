f509727bd8db16a29cf727cd3a9934f26b96d8d5
ed-cavalcanti <edney2809@gmail.com>
2023-05-16 20:48:48 -0300

    [feat] stripes line

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden">
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 rounded-full translate-x-1/2 bg-purple-700 opacity-50 blur-full" />
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+        <div id="blur"  className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 rounded-full translate-x-1/2 bg-purple-700 opacity-50 blur-full" />
+        <div id="stripes" className="absolute right-2 top-0 bottom-0 w-2 bg-stripes " />
       </div>
 
       {/* rigth */}
