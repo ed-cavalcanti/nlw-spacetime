be50c03a6f20bd293d0920578f7c910e7ee72d9a
ed-cavalcanti <edney2809@gmail.com>
2023-05-16 20:54:40 -0300

    [feat] stars background

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div id="blur"  className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 rounded-full translate-x-1/2 bg-purple-700 opacity-50 blur-full" />
         <div id="stripes" className="absolute right-2 top-0 bottom-0 w-2 bg-stripes " />
       </div>
 
       {/* rigth */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">Você ainda não criou nenhuma lembrança, comece a{' '}
           <a href="" className="underline hover:text-gray-50">criar agora</a>
