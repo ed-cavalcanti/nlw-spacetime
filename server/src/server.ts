b1306273a82f65442f8a7e14bf0f4b6e15016882
ed-cavalcanti <edney2809@gmail.com>
2023-05-16 19:25:02 -0300

    [build] app initial setup

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const port = 3333
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app
+  .listen({
+    port,
+  })
+  .then(() => {
+    console.log(`Server running on http://localhost:${port}`)
+  })
