eccb1a874253e8edee70c205a1e7cced7a4ffd0e
ed-cavalcanti <edney2809@gmail.com>
2023-05-17 10:18:18 -0300

    [feat] app homepage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { styled } from 'nativewind'
 
 import {
@@ -12,6 +12,7 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
+import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
 
 const StyledStripes = styled(Stripes)
 
@@ -33,9 +34,34 @@ export default function App() {
         left: '-100%',
       }}
       source={blurBg}
-      className="relative flex-1 bg-gray-950"
+      className="relative flex-1 bg-gray-950 px-8 py-10"
     >
       <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar style="auto" />
     </ImageBackground>
   )
