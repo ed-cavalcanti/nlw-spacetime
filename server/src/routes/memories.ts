00b28b9f6ee9d535598bf27ed73668a2ebc85d9e
ed-cavalcanti <edney2809@gmail.com>
2023-05-17 11:39:35 -0300

    [feat] post memory route

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -35,8 +35,25 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.post('/memories', async () => {
-    
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { coverUrl, content, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '91fe7496-ee1d-45ef-aadb-d46d9fe8e127',
+      },
+    })
+
+    return memory
   })
 
   app.put('/memories:id', async () => {
