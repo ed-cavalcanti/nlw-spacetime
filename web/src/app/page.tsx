ef2fd69e1ef7e60d3a36a4a095fa4108c727f25f
ed-cavalcanti <edney2809@gmail.com>
2023-05-16 21:11:31 -0300

    [feat] SignIn section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { User } from 'lucide-react'
+
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
@@ -5,13 +7,23 @@ export default function Home() {
       <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div id="blur"  className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 rounded-full translate-x-1/2 bg-purple-700 opacity-50 blur-full" />
         <div id="stripes" className="absolute right-2 top-0 bottom-0 w-2 bg-stripes " />
-      </div>
 
+        {/* SignIn */}
+        <a href="" className="flex items-center gap-3 text-left hover:text-white transition-colors">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="text-sm leading-snug max-w-[140px]">
+            <span className="underline">Crie sua conta</span>{' '}
+            e salve suas memórias!
+          </p>
+        </a>
+      </div>
       {/* rigth */}
       <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">Você ainda não criou nenhuma lembrança, comece a{' '}
-          <a href="" className="underline hover:text-gray-50">criar agora</a>
+          <a href="" className="underline hover:text-white transition-colors">criar agora</a>
           !
           </p>
         </div>
