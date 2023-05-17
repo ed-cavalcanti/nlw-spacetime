2e36e7e0e0398af0fd01f74bc64d9efb6d732d17
ed-cavalcanti <edney2809@gmail.com>
2023-05-17 09:33:07 -0300

    [config] tailwind colors

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-4xl font-bold text-gray-50">Spacetime</Text>
+      <Text className="text-4xl font-bold text-gray-100">Spacetime</Text>
       <StatusBar style="auto" />
     </View>
   )
