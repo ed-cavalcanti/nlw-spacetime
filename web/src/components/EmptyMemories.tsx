6ffbb10f2918b6930fd2b5e3200b63747b28a8b6
ed-cavalcanti <edney2809@gmail.com>
2023-05-18 11:50:22 -0300

    [refact] makes part of homepage into components

---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,10 @@
+export default function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="text-center leading-relaxed w-[360px]">Você ainda não criou nenhuma lembrança, comece a{' '}
+      <a href="" className="underline hover:text-white transition-colors">criar agora</a>
+      !
+      </p>
+    </div>
+  )
+}
